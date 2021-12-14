90690f33bbdb45787efb13cb490882e4c3603d82
GailSlaney <gailslaney258@gmail.com>
2021-12-14 19:49:12 -0330

    added code to js file

---
+++ script.js
@@ -0,0 +1,31 @@
+let startTime;
+let elapsedTime;
+
+//Starts counting time when start button is clicked.
+function start() {
+    startTime = Date.now();
+    setInterval(function printTime() {
+        elapsedTime = Date.now() - startTime;
+        document.getElementById("display").innerHTML = timeToString(elapsedTime);
+    }, 10);
+}
+
+function timeToString(time) {
+    let diffInHrs = time / 3600000;
+    let hh = Math.floor(diffInHrs);
+
+    let diffInMin = (diffInHrs - hh) * 60;
+    let mm = Math.floor(diffInMin);
+
+    let diffInSec = (diffInMin - mm) * 60;
+    let ss = Math.floor(diffInSec);
+
+    let diffInMs = (diffInSec - ss) * 100;
+    let ms = Math.floor(diffInMs);
+
+    let formattedHH = hh.toString().padStart(2, "0");
+    let formattedMM = mm.toString().padStart(2, "0");
+    let formattedSS = ss.toString().padStart(2, "0");
+    let formattedMS = ms.toString().padStart(3, "0");
+    return `${formattedHH}:${formattedMM}:${formattedSS}:${formattedMS}`;
+}
